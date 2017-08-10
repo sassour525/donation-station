9b4963ed12d081571dd9d3cb4f6b171257e1ced0
Shawn Assour <sassour@Shawns-MacBook-Pro.local>
2017-08-10 13:15:06 -0500

    fixed DB model for items

--- models/item_price.js
+++ models/item_price.js
@@ -1,9 +1,8 @@
 module.exports = function(sequelize, dataTypes) {
-	var Donation_Item = sequelize.define('Donation_Item', {
+	var Item_Price = sequelize.define('Item_Price', {
 		category: dataTypes.STRING,
 		item_name: dataTypes.STRING,
-		type: dataTypes.STRING,
 		price: dataTypes.DECIMAL
 	});
-	return Donation_Item;
+	return Item_Price;
 }
